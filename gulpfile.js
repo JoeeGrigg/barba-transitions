d287917e892510eba7392d1d35ff49c143903cae
M. Folkers <hello@twobrain.io>
2017-09-03 10:22:40 +0200

    Adding example javascript output to gulp install task

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,7 @@ gulp.task('js', function(){
     .pipe(sourcemaps.init())
     .pipe(concat('barba.transitions.js'))
     .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./example/temp'))
     .pipe(concat('barba.transitions.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist'))
